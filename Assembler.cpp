5b7b4e01055debc71f9bf7e7f4e3e79ccd4c8e23
tiezhu-lee <lk0219@foxmail.com>
2016-02-06 22:25:58 +0800

    EraseComment() accomplished.

--- Assembler.cpp
+++ Assembler.cpp
@@ -3,6 +3,22 @@
 //#include "label.h"
 using namespace std;
 
+void Assembler::EraseComment()
+{
+	for(auto it = lines.begin(); it != lines.end(); it++ ) {
+		int pos1 = it -> find("//");
+		int pos2 = it -> find('#');
+		int pos;
+		if(pos1 == string::npos && pos2 == string::npos) continue;
+		if(pos1 == string::npos || pos2 == string::npos) 
+			pos = (pos1 == string::npos)?pos2:pos1;
+		else
+			pos = (pos1 < pos2 )?pos1:pos2;
+
+		it -> erase(pos, it->length()-pos+1);
+	}
+}
+
 void Assembler::Format()
 {
 	/*
@@ -11,15 +27,21 @@ void Assembler::Format()
 	 * 	erase white blanks at the beginning and end of each line;
 	 *	put the label in a new line;
 	 */
-	auto it = lines.begin();
-	while(it != lines.end() )
-	{
+	for(auto it = lines.begin(); it != lines.end(); it++ ) {
+		int pos = find(':');
+		if(pos == string::npos) continue;
+		string buf = it -> substr(pos+1, it -> length()-pos);
+		if(buf.empty()) continue;
+		it->erase(pos+1, it -> length()-pos);
+		lines.insert(++it, buf);
+	}
+
+	while(it != lines.end() ) {
 		for(auto str_it = it -> begin(); str_it != it -> end(); ) {
 			if(*str_it == ' ' || *str_it == '\t')
 				it -> erase(str_it);
 			else break;
 		}
-
 		auto str_rit = it -> rbegin();
 		for( ; *str_rit == ' ' || *str_rit == '\t' ; str_rit ++ ) ;
 		string::iterator str_it(str_rit.base() );
@@ -28,8 +50,6 @@ void Assembler::Format()
 		if(it -> empty()) lines.erase(it);
 		else it++;
 	}
-
-
 }
 
 void Assembler::PreProcess()
